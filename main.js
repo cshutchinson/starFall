1a41b70f6b51c37ec8326c14ac97c69f2f016bdc
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-02 08:32:30 -0700

    random movement complete with 500 particles

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var particleArray = new Array;
 
-for (var x=0; x<50; x++){
+for (var x=0; x<500; x++){
   particleArray.push(new Particle);
 }
 
 function Particle() {
-  this.x= Math.floor(Math.random()*canvas.width);
-  this.y= Math.floor(Math.random()*50);
-  this.vx= 0;
-  this.vy= (Math.random()*5)+2;
-  this.size= 5;
+  this.x = Math.floor(Math.random()*canvas.width);
+  this.y = Math.floor(Math.random()*50);
+  this.vx = Math.tan((Math.random()*5));
+  this.vy = (Math.random()*5)+2;
+  this.size= 3;
   this.color= '#'+Math.floor(Math.random()*16777215).toString(16);
   this.drawParticle = function() {
     ctx.fillStyle = this.color;
@@ -27,7 +27,8 @@ function drawCanvas() {
     elem.x += elem.vx;
     elem.y += elem.vy;
     elem.drawParticle();
-    // elem.x >= canvas.height ? elem.x=0 : null;
+     elem.y >= canvas.height ? elem.y=0 : null;
+     elem.x >= canvas.width ? elem.x=0 : null;
   });
   raf = window.requestAnimationFrame(drawCanvas);
 }
